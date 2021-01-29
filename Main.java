daa0ddfe9ba85d1546bad895f283ecf70c6ef1ab
ISlack7 <ian.slack@patelhs.org>
2021-01-29 16:27:15 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) 
+	{
+    Scanner scan = new Scanner(System.in);
+		System.out.println("What is your name?");
+		String name = scan.next();
+		System.out.println("Hi, " + name + " What year did you start school?");
+		int yearStartedSchool = scan.nextInt();
+		System.out.println(name + ", what year is it?");
+		int currentYear = scan.nextInt();
+
+		int years = numYears(yearStartedSchool, currentYear);
+
+		System.out.println(junior(years));
+  }
+	//numYears method gets how many years you have been in school for.
+
+	static int numYears(int yearStartedSchool, int currentYear)
+	{
+		int yearsToGo = yearStartedSchool - currentYear;
+		return yearsToGo;
+	}
+	// junior method determins if you are a junior or not.
+	static boolean junior(int yearsToGo){
+		if (yearsToGo == 2){
+			return true;
+		} else{
+			return false;
+		}
+	}
+	
+}
